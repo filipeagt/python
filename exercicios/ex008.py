31b31bfaa08f537f3dfb0e347e46713c3a613d8e
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-06 17:00:07 -0300

    Update ex008.py

--- exercicios/ex008.py
+++ exercicios/ex008.py
@@ -1,4 +1,8 @@
-m = float(input('Digite um valor em metros: '))
+m = float(input('Uma distância em metros: '))
+km = m/1000
+hm = m/100
+dam = m/10
+dm = m*10
 cm = m*100
 mm = m*1000
-print('O comprimento de {}m, corresponde a {}cm ou {}mm'.format(m, cm, mm))
+print('A medida de {:.1f}m corresponde a \n{}km \n{}hm \n{}dam \n{:.0f}dm \n{:.0f}cm \n{:.0f}mm'.format(m, km, hm, dam, dm, cm, mm))
