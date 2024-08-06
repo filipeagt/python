cf6f922011a2f26ef50318b41853b5adceaf012e
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-06 17:21:01 -0300

    Update ex010.py

--- exercicios/ex010.py
+++ exercicios/ex010.py
@@ -1,2 +1,4 @@
-valor = float(input('Quantos reais você tem na carteira? '))
-print('Com R${:.2f} você pode comprar US${:.2f}'.format(valor, valor*3.27))
+real = float(input('Quanto dinheiro você tem na carteira? R$'))
+cotacao = 5.66
+dolar = real / cotacao
+print('Com R${:.2f} você pode comprar US${:.2f}'.format(real, dolar))
