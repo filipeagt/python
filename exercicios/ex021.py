b95f96442aa668f6bc7619efc156942390770efb
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-08 15:56:06 -0300

    Update ex021.py

--- exercicios/ex021.py
+++ exercicios/ex021.py
@@ -1,6 +1,6 @@
-from pygame import mixer as mp3
-mp3.init()
-mp3.music.load('happy-mistake.mp3')
-mp3.music.play()
-while mp3.music.get_busy() == True:
-    continue
+from pygame import mixer, event
+mixer.init()
+mixer.music.load('happy-mistake.mp3')
+mixer.music.play()
+input()
+event.wait()
