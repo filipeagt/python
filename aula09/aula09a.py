e82ebce4898cc0fa885e1daee7e31d86afa009c5
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-09 16:55:31 -0300

    Create aula09a.py

---
+++ aula09/aula09a.py
@@ -0,0 +1,8 @@
+frase = 'Curso em Vídeo Python'
+print(frase[::2])
+print(frase.upper().count('O'))
+frase = frase.replace('Python', 'Android')
+print(frase)
+print('Curso' in frase)
+print(frase.find('em'))
+print(frase.split())
