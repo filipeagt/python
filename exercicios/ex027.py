7ea08b2567849821112ebcd0e24c96eb0f1baf0f
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-10 16:37:46 -0300

    Update ex027.py

--- exercicios/ex027.py
+++ exercicios/ex027.py
@@ -1,3 +1,4 @@
-nome = input("Digite um nome completo: ")
-print('Primeiro nome: {}'.format(nome.split()[0]))
-print('Último nome: {}'.format(nome.split()[-1]))
+nome = input("Digite um nome completo: ").split()
+print('Prazer em te conhecer!')
+print('Primeiro nome: {}'.format(nome[0]))
+print('Último nome: {}'.format(nome[-1]))
