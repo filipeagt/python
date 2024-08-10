1d0e7ae075bbc33532e4d9992536a5d157f347e6
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-10 16:31:22 -0300

    Update ex026.py

--- exercicios/ex026.py
+++ exercicios/ex026.py
@@ -1,8 +1,3 @@
-frase = input('Digite uma frase: ').upper()
-
-for pos in range(len(frase)-1, -1, -1):    
-    if frase[pos] == 'A':
-        break
-
+frase = input('Digite uma frase: ').upper().strip()
 print('A letra "A" aparece {} vezes na frase.'.format(frase.count('A')))
-print('A letra "A" aparece pela primeira vez na posição {}, e pela última vez na posição {}.'.format(frase.find('A'), pos))
+print('A letra "A" aparece pela primeira vez na posição {}, e pela última vez na posição {}.'.format(frase.find('A'), frase.rfind('A')))
