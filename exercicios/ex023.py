7ff32ddf22a5932487e374633e6a6604066e0c0a
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-10 16:09:21 -0300

    Update ex023.py

--- exercicios/ex023.py
+++ exercicios/ex023.py
@@ -1,5 +1,6 @@
 num = input('Digite um número entre 0 e 9999: ')
 num = num.zfill(4)
+print('Analizando o número {}.'.format(num))
 print('Unidade: {}'.format(num[3]))
 print('Dezena:  {}'.format(num[2]))
 print('Centena: {}'.format(num[1]))
