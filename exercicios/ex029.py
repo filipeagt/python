e3a7e78414b13a08a11ee261273ddc623cd654a3
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-11 20:44:27 -0300

    Update ex029.py

--- exercicios/ex029.py
+++ exercicios/ex029.py
@@ -2,4 +2,6 @@ vel = float(input('Qual a velocidade do carro? (km/h) '))
 if vel > 80:
     diferenca = vel - 80
     multa = diferenca * 7
-    print('Você ultrapassou {}km/h do limite da via! MULTADO! R${:.2f}'.format(diferenca, multa))
+    print('Você ultrapassou {}km/h do limite de velocidade! MULTADO! R${:.2f}'.format(diferenca, multa))
+
+print('Tenha um bom dia! Dirija com segurança!')
