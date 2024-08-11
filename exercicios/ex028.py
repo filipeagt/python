940ac866895c6e0e66f68dd206e2661fc0ab3b9f
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-11 20:34:28 -0300

    Update ex028.py

--- exercicios/ex028.py
+++ exercicios/ex028.py
@@ -1,4 +1,12 @@
 from random import randint
-aleatorio = randint(0,5)
-num = int(input('Pensei num número entre 0 e 5, tente adivinhar... '))
+from time import sleep
+aleatorio = randint(0, 5)
+linha = '-=-'*20
+print(linha)
+print('Vou pensar em um número entre 0 e 5, tente adivinhar... ')
+print(linha)
+num = int(input('Em que número eu pensei? '))
+print('PROCESSANDO...')
+sleep(2)
 print('Acertou Mizeravi!' if num == aleatorio else 'ERROU!')
+print('Pensei no número {}.'.format(aleatorio))
