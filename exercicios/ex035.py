fbd88dd021a9e3626975a3083d39b43d94fe3b85
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-12 19:31:18 -0300

    Update ex035.py

--- exercicios/ex035.py
+++ exercicios/ex035.py
@@ -1,7 +1,11 @@
+linha = '-=-'*10
+print(linha)
+print('Analisador de trângulos')
+print(linha)
 r1 = float(input('Comprimento da primeira reta: '))
 r2 = float(input('Comprimento da segunada reta: '))
 r3 = float(input('Comprimento da terceira reta: '))
-if r1>r2+r3 or r2>r1+r3 or r3>r1+r2:
+if r1>=r2+r3 or r2>=r1+r3 or r3>=r1+r2:
     print('As retas {}, {} e {}, não podem formar um triângulo.'.format(r1, r2, r3))
 else:
     print('As retas {}, {} e {}, formam um triângulo.'.format(r1, r2, r3))
