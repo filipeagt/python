f3b9648d76a754c5f6189f29f88e114e9d97f29d
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-11 21:00:50 -0300

    Update ex031.py

--- exercicios/ex031.py
+++ exercicios/ex031.py
@@ -1,6 +1,7 @@
 dist = int(input('Informe a distância da viagem em km: '))
+print('Você está prestes a começar uma viagem de {}km.'.format(dist))
 if dist <= 200:
     passagem  = dist * .50
 else:
     passagem = dist * .45
-print('O valor da passagem será {:.2f}.'.format(passagem))
+print('O valor da passagem será R${:.2f}.'.format(passagem))
