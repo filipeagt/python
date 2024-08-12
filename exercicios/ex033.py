d2dd76f0311320c64728222a5686167d26b8b394
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-11 21:17:22 -0300

    Update ex033.py

--- exercicios/ex033.py
+++ exercicios/ex033.py
@@ -1,5 +1,5 @@
 numeros = []
 for x in range(3):
     numeros.append(int(input('Informe o {}º número: '.format(x+1))))
-print('O maior número é {}'.format(max(numeros)))
 print('O menor número é {}'.format(min(numeros)))
+print('O maior número é {}'.format(max(numeros)))
