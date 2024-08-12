2d765b7725c4ac1a307c3fea20ea00230ced90d0
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-11 21:11:28 -0300

    Update ex032.py

--- exercicios/ex032.py
+++ exercicios/ex032.py
@@ -1,4 +1,7 @@
-ano = int(input('Informe um ano: '))
+from datetime import date
+ano = int(input('Informe um ano ou coloque 0 para analisar o ano atual): '))
+if ano == 0:
+    ano = date.today().year
 if ano % 4 != 0 or ano % 100 == 0 and ano % 400 != 0:
     print('O ano de {} não é bissexto'.format(ano))
 else:
