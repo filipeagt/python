eb3e6a045e9add520bc868df01f8404fca0f83b5
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-15 00:37:06 -0300

    Update ex037.py

--- exercicios/ex037.py
+++ exercicios/ex037.py
@@ -1,14 +1,15 @@
 print('Calculadora de bases numéricas')
 num =  int(input('Digite um número inteiro: '))
-print('- Digite 1 para binário')
-print('- Digite 2 para octal')
-print('- Digite 3 para hexadecimal')
+print('Escolha uma das bases para conversão:')
+print('[ 1 ] converter para binário')
+print('[ 2 ] converter para octal')
+print('[ 3 ] converter para hexadecimal')
 opcao = int(input('Opção: '))
 if opcao == 1:
     print('O valor {} em binário é {}'.format(num, bin(num)[2:]))
 elif opcao == 2:
     print('O valor {} em octal é {}'.format(num, oct(num)[2:]))
 elif opcao == 3:
-    print('O valor {} em hexadecimal é {}'.format(num, hex(num)[2:]))
+    print('O valor {} em hexadecimal é {}'.format(num, hex(num)[2:].upper()))
 else:
     print('Opção inválida')
