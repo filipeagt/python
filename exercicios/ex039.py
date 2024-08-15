1a4c70c23a2174f67d5da76d7b6ce957851513d6
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-15 01:11:25 -0300

    Update ex039.py

--- exercicios/ex039.py
+++ exercicios/ex039.py
@@ -2,12 +2,23 @@ from datetime import date
 anoAtual = date.today().year
 print('Alistamento Militar')
 nascimento = int(input('Qual o ano de nascimento: '))
+sexo = str(input('Sexo: (F, M)')).upper()
 idade = anoAtual - nascimento
-if(idade < 18):
-    prazo = 18 - idade
-    print('Você ainda vai se alistar, faltam {} anos'.format(prazo) if prazo > 1 else 'Você ainda vai se alistar, falta 1 ano')
-elif idade == 18:
-    print('Está na hora de se alistar')
+alistamento = nascimento + 18
+print('Quem nasceu em {} faz {} anos em {}'.format(nascimento, idade, anoAtual))
+if sexo == 'M':
+    if(idade < 18):
+        prazo = 18 - idade
+        print('Você ainda vai se alistar, faltam {} anos'.format(prazo) if prazo > 1 else 'Você ainda vai se alistar, falta 1 ano')
+        print('Seu alistamento será em {}'.format(alistamento))
+    elif idade == 18:
+        print('Está na hora de se alistar')
+    else:
+        prazo = idade - 18
+        print('Passaram {} anos do prazo'.format(prazo) if prazo > 1 else 'Passou 1 ano do prazo')
+        print('Seu alistamento foi em {}'.format(alistamento))
+elif sexo == 'F':
+    print('O alistamento NÃO é obrigatório para o sexo feminino.')
 else:
-    prazo = idade - 18
-    print('Passaram {} anos do prazo'.format(prazo) if prazo > 1 else 'Passou 1 ano do prazo')
+    print('Opção inválida.')
+
