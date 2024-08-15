1643a17525fa72c1988a9d057466c262b0774ff7
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-15 00:42:06 -0300

    Update ex038.py

--- exercicios/ex038.py
+++ exercicios/ex038.py
@@ -1,8 +1,8 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 if n1 > n2:
-    print('O primeiro valor é maior.')
+    print('O PRIMEIRO valor é maior.')
 elif n2 > n1:
-    print('O segundo valor é maior.')
+    print('O SEGUNDO valor é maior.')
 else:
-    print('Os dois valores são iguais.')
+    print('Os dois valores são IGUAIS.')
