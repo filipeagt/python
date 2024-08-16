2938480c8440c264321c6be0aa15b85e1aaca59d
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-15 22:56:05 -0300

    Update ex040.py

--- exercicios/ex040.py
+++ exercicios/ex040.py
@@ -1,10 +1,10 @@
 cores = {'vermelho':'\033[1;31m', 'verde':'\033[1;32m', 'amarelo':'\033[1;33m', 'azul':'\033[1;34m', 'limpa':'\033[m'}
 linha = '-=-'*10
-print('{}{}\nCalculo de média\n{}{}'.format(cores['azul'], linha, linha, cores['limpa']))
+print('{}{}\n      Calculo de média\n{}{}'.format(cores['azul'], linha, linha, cores['limpa']))
 n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digite a seguda nota: '))
 media  = (n1 + n2) / 2
-print('Média = {}'.format(media))
+print('Tirando {:.1f} e {:.1f}, a média do aluno é {:.1f}'.format(n1, n2, media))
 if media < 5:
     print('{}REPROVADO{}'.format(cores['vermelho'], cores['limpa']))
 elif media < 7:
