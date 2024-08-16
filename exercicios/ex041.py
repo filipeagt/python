42526e0ab9a992d88d1ded4dae459605146cf6a5
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-16 15:28:35 -0300

    Update ex041.py

--- exercicios/ex041.py
+++ exercicios/ex041.py
@@ -1,15 +1,19 @@
+from datetime import date
 cores = {'vermelho':'\033[1;31m', 'verde':'\033[1;32m', 'amarelo':'\033[1;33m', 'azul':'\033[1;34m', 'limpa':'\033[m'}
 linha = '-=-'*10
 print('{}{}\nConfederação Nacional de Natação\n{}{}'.format(cores['azul'], linha, linha, cores['limpa']))
-idade = int(input('Informe a idade do atleta: '))
+nascimento = int(input('Ano de nascimento do atleta:'))
+anoAtual = date.today().year
+idade = anoAtual - nascimento
 if idade <= 9:
     categoria = 'MIRIM'
 elif idade <= 14:
     categoria = 'INFANTIL'
 elif idade <= 19:
     categoria = 'JUNIOR'
-elif idade <= 20:
+elif idade <= 25:
     categoria = 'SÊNIOR'
 else:
     categoria = 'MASTER'
-print('Categoria do atleta: {}'.format(categoria))
+print('O atleta tem {} anos.'.format(idade))
+print('Categoria: {}'.format(categoria))
