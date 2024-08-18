198a383a492af0181faa58a31c04b294908313e1
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-18 11:08:43 -0300

    Exercício 56
    Cadastro de pessoas

--- exercicios/ex056.py
+++ exercicios/ex056.py
@@ -1,1 +1,24 @@
-#33:16
+print('Cadastro de pessoas')
+pessoas = list()
+mediaIdade = 0
+menos20 = 0
+idadeVelho = 0
+NCADASTROS = 4
+for x in range(NCADASTROS):
+    nome = str(input('Nome: ')).strip()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo (F ou M): ')).upper().strip()
+    pessoas.append({'nome':nome, 'idade':idade, 'sexo':sexo})
+
+for pessoa in pessoas:
+    mediaIdade += pessoa['idade']
+    if pessoa['sexo'] == 'F' and pessoa['idade'] < 20:
+        menos20 += 1
+    if pessoa['sexo'] == 'M' and pessoa['idade'] > idadeVelho:
+        idadeVelho = pessoa['idade']
+        nomeVelho = pessoa['nome']
+mediaIdade /= NCADASTROS
+
+print('Média das idadades: {} anos'.format(mediaIdade))
+print('Homem mais velho: {}'.format(nomeVelho))
+print('Mulheres com menos de 20 anos: {}'.format(menos20))
