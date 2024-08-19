cb3640bff024face19fba026b6610df0d055813e
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-19 18:14:53 -0300

    Update ex050.py

--- exercicios/ex050.py
+++ exercicios/ex050.py
@@ -1,6 +1,8 @@
 soma = 0
+cont = 0
 for x in range(6):
     num =  int(input('Digite o {}º número: '.format(x+1)))
     if num%2 == 0:
         soma += num
-print(soma)
+        cont += 1
+print('Você informou {} números pares e a soma deles foi {}.'.format(cont, soma))
