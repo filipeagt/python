ef932e31fdc9e9bdb7c88fee345c21e2d22786a8
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-21 18:23:29 -0300

    Update ex052.py

--- exercicios/ex052.py
+++ exercicios/ex052.py
@@ -1,10 +1,13 @@
 num = int(input('Digite um número: '))
-primo = True
-for x in range(2, num):
+divisivel = 0
+for x in range(1, num+1):
     if num % x == 0:
-        primo = False
-        break
-if primo:
-    print('O número {} é PRIMO.'.format(num))
+        print('\033[33m{}\033[m'.format(x),end=' ')
+        divisivel += 1
+    else:
+        print('\033[31m{}\033[m'.format(x), end=' ')
+print('\nO número {} foi divisível {} vezes.'.format(num, divisivel))
+if divisivel == 2:
+    print('E por isso ele é PRIMO!'.format(num))
 else:
-    print('O número {} NÃO é primo.'.format(num))
+    print('E por isso ele NÃO é primo!'.format(num))
