32194dd66d6338ce018989300887a6b66b426f3f
Filipe Almeida <filipeagt.92@gmail.com>
2024-08-29 17:10:37 -0300

    Update ex056.py

--- exercicios/ex056.py
+++ exercicios/ex056.py
@@ -5,6 +5,7 @@ menos20 = 0
 idadeVelho = 0
 NCADASTROS = 4
 for x in range(NCADASTROS):
+    print('----- {}ª Pessoa -----'.format(x+1))
     nome = str(input('Nome: ')).strip()
     idade = int(input('Idade: '))
     sexo = str(input('Sexo (F ou M): ')).upper().strip()
@@ -19,6 +20,6 @@ for pessoa in pessoas:
         nomeVelho = pessoa['nome']
 mediaIdade /= NCADASTROS
 
-print('Média das idadades: {} anos'.format(mediaIdade))
-print('Homem mais velho: {}'.format(nomeVelho))
-print('Mulheres com menos de 20 anos: {}'.format(menos20))
+print('A média das idadades é {} anos.'.format(mediaIdade))
+print('O homem mais velho tem {} anos e se chama {}.'.format(idadeVelho, nomeVelho))
+print('Existem {} mulheres com menos de 20 anos no grupo.'.format(menos20))
