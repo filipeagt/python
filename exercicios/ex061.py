fc25b5c40b36c5dc0d89c237ddc2bdc31a3d39c5
Filipe Almeida <filipeagt.92@gmail.com>
2024-11-10 20:17:44 -0300

    desafio 61

---
+++ exercicios/ex061.py
@@ -0,0 +1,11 @@
+print('='*25)
+print('{:^25}'.format('10 Termos de uma PA'))
+print('='*25)
+num = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+i = 0
+while i < 10:
+    print('{} =>'.format(num), end=' ')
+    num += razao
+    i += 1
+print('Acabou')
