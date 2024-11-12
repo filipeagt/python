2035beb69e6523264c0930634fbaaa08c82a26ab
Filipe Almeida <filipeagt.92@gmail.com>
2024-11-11 22:50:07 -0300

    Vídeo desafio 57

--- exercicios/ex057.py
+++ exercicios/ex057.py
@@ -1,3 +1,4 @@
-sexo = 'x'
-while sexo not in 'mMfF':
-    sexo = str(input('Informe o sexo da pessoa [F/M]: '))
+sexo = str(input('Informe o sexo da pessoa [F/M]: ')).strip().upper()[0]
+while sexo not in 'MF':
+    sexo = str(input('Dado inválido, informe o sexo [F/M]: ')).strip().upper()[0]
+print('Sexo {} registrado com sucesso.'.format('Masculino' if sexo=='M' else 'Feminino'))
