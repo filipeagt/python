ac55cb06afa1ebe897b000401a788373ddf15329
Filipe Almeida <filipeagt.92@gmail.com>
2024-11-13 00:01:15 -0300

    Vídeo desafio 59

--- exercicios/ex059.py
+++ exercicios/ex059.py
@@ -1,7 +1,7 @@
 opcao = 4
-while (opcao != 5):
-    num1 = int(input('Digite um número: '))
-    num2 =  int(input('Digite outro número: '))
+num1 = int(input('Digite um número: '))
+num2 =  int(input('Digite outro número: '))
+while (opcao != 5):    
     opcao = int(input('----Menu----\n[1]somar\n[2]multiplicar\n[3]maior\n[4]novos números\n[5]sair\n'))
     if opcao == 1:
         print('A soma entre {} e {} é igual a {}.'.format(num1, num2, num1 + num2))
@@ -14,9 +14,12 @@ while (opcao != 5):
         print('O maior valor entre {} e {} é {}'.format(num1, num2, maior))
     elif opcao == 4:
         print('Escolha novos valores!')
+        num1 = int(input('Digite um número: '))
+        num2 =  int(input('Digite outro número: '))
         continue
     elif opcao == 5:
         print('Até logo!')
         break
     else:
         print('Opção inválida!')
+print('Fim do programa!')
