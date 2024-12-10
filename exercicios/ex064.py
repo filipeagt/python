a1eeea08b88b32d80318ecb3df4085ccab97e632
Filipe Almeida <filipeagt.92@gmail.com>
2024-12-10 16:52:47 -0300

    Vídeos dos desafios 63 e 64

--- exercicios/ex064.py
+++ exercicios/ex064.py
@@ -1,10 +1,8 @@
-n = 0
-soma = 0
-cont = 0
-
-while n != 999:
+soma = cont = 0
+num = int(input('Digite um número [999 para parar]: '))
+while num != 999:
     cont  += 1
-    soma += n
-    n = int(input('Digite um número: '))
+    soma += num
+    num = int(input('Digite um número [999 para parar]: '))
     
-print('Ao todo foram digitados {} números e a soma entre eles é {}'.format(cont-1, soma))
+print('Ao todo foram digitados {} números e a soma entre eles é {}'.format(cont, soma))
