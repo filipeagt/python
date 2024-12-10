688f2b736f7f3a2c50e69bf926aea372433d2bfc
Filipe Almeida <filipeagt.92@gmail.com>
2024-12-10 17:51:30 -0300

    Vídeo desafio 65

--- exercicios/ex065.py
+++ exercicios/ex065.py
@@ -1,23 +1,17 @@
 continuar = 'S'
-soma = 0
-cont = 0
-maior = 0
-menor = 0
-inicio = True
-
+soma = cont = media = maior = menor = 0
 while continuar == 'S':
     n = int(input('Digite um número: '))
     cont += 1
     soma += n
-    if inicio:
-        inicio = False
-        maior = n
-        menor = n
+    if cont == 1:
+        maior = menor = n
     else:
         if n > maior:
             maior = n
         if n < menor:
             menor = n
-    continuar = input('Quer continuar? [S/N] ').upper()
-print('A média dos valores digitados foi {}'.format(soma/cont))
+    continuar = input('Quer continuar? [S/N] ').upper().strip()[0]
+media =  soma / cont
+print('Você digitou {} números e a média foi {}'.format(cont, media))
 print('O maior valor digitado foi {} e o menor foi {}'.format(maior, menor))
