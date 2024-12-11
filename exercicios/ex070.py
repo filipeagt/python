b36427f16c9f20fa64d4b02a3d753b10c189c8d1
Filipe Almeida <filipeagt.92@gmail.com>
2024-12-11 19:04:38 -0300

    Vídeo desafio 70

--- exercicios/ex070.py
+++ exercicios/ex070.py
@@ -1,33 +1,27 @@
 produtos = []
 maisBararto = CustaMaisDeMil = total = 0
 nomeMaisBarato = ''
-print('=-'*10)
-print('Carrinho de Compras')
-print('=-'*10)
+print('=-'*20)
+print(f'{"Carrinho de Compras":^40}')
+print('=-'*20)
 while True:
-    nome = input('Nome: ').strip()
-    preço = float(input('Preço (R$): '))
+    nome = input('Nome do produto: ').strip().capitalize()
+    preço = float(input('Preço: R$ '))
     produtos.append({'nome': nome, 'preço': preço})
-    continuar = ''
-    while True:
+    continuar = ' '
+    while continuar not in 'SN':
         continuar = input('Deseja continuar? [S/N] ').upper().strip()[0]
-        if continuar in 'SN':
-            print('-'*20)
-            break
+    print('-'*40)
     if continuar == 'N':
         break
 for produto in produtos:
     total += produto['preço']
     if produto['preço'] > 1000:
         CustaMaisDeMil += 1
-    if produto == produtos[0]: #Primeira iteração
+    if produto == produtos[0] or produto['preço'] < maisBararto: #Primeira iteração ou nas demais iterações se o preço for menor
         maisBararto = produto['preço']
         nomeMaisBarato = produto['nome']
-    else: #Demais iterações
-        if produto['preço'] < maisBararto:
-            maisBararto = produto['preço']
-            nomeMaisBarato = produto['nome']
-print('-'*10, f'FIM DO PROGRAMA', '-'*10)
+print(f'{" FIM DO PROGRAMA ":-^40}')
 print(f'O total gasto na compra foi R${total:.2f}.'.replace('.',',',1))
-print(f'{CustaMaisDeMil} produto(s) custa(m) mais de R$1000,00.')
+print(f'Temos {CustaMaisDeMil} produto(s) que custa(m) mais de R$1000,00.')
 print(f'O produto "{nomeMaisBarato}" foi o mais barato custando R${maisBararto:.2f}.'.replace('.', ',', 1))
