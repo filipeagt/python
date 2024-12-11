a96de558cc8b09ff8b88f8ac74c33430bfa77660
Filipe Almeida <filipeagt.92@gmail.com>
2024-12-11 17:28:46 -0300

    Vídeo desafio 67

--- exercicios/ex067.py
+++ exercicios/ex067.py
@@ -1,9 +1,8 @@
-num = 0
 while True:
-    num = int(input("Qual tabuada vc quer ver? [digite -1 para sair]"))
-    if num < 0:
-        break
+    num = int(input("Qual tabuada vc quer ver? [digite um valor negativo para sair] "))
     print('-'*12)
+    if num < 0:
+        break    
     for x in range(1, 11):
         print(f'{num} x {x:2} = {num*x}')
     print('-'*12)
