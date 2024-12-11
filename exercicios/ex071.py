1a24cc83a3cfe1f0e9b8667255d20fd52b239511
Filipe Almeida <filipeagt.92@gmail.com>
2024-12-11 17:08:16 -0300

    Desafio 71

---
+++ exercicios/ex071.py
@@ -0,0 +1,21 @@
+n50 = n20 = n10 = n1 = valor = 0
+print('='*30)
+print(f'{"Caixa eletrônico":^30}')
+print('='*30)
+valor = int(input('Qual o valor a ser sacado? R$'))
+n50 = int(valor / 50)
+n20 = int(valor % 50 / 20)
+n10 = int(valor % 50 % 20 / 10)
+n1  = valor % 50 % 20 % 10
+print('='*30)
+print(f'Saque realizado: ')
+if n50 > 0:
+    print(f'{n50} cédula(s) de R$50')
+if n20 > 0:
+    print(f'{n20} cédula(s) de R$20')
+if n10 > 0:
+    print(f'{n10} cédula(s) de R$10')
+if n1 > 0:
+    print(f'{n1} cédula(s) de R$1')
+print('='*30)
+print('Volte sempre! Tenha um bom dia!')
