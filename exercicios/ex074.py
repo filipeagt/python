286d1591ece36868f6fc3801c42c04d45197f680
Filipe Almeida <filipeagt.92@gmail.com>
2024-12-12 20:15:10 -0300

    Vídeo desafio 74

--- exercicios/ex074.py
+++ exercicios/ex074.py
@@ -1,15 +1,6 @@
 from random import randint
-lista = []
-menor = maior = 0
-for x in range(5):
-    lista.append(randint(0, 10))
-    if x == 0:
-        menor = maior = lista[x]
-    else:
-        if lista[x] < menor:
-            menor = lista[x]
-        if lista[x] > maior:
-            maior = lista[x]
-tupla = tuple(lista)
-print(f'Número sorteados: {tupla}')
-print(f'O menor valor na tupla é {menor} e o maior é {maior}.')
+tupla = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+print(f'Número sorteados: ', end='')
+for num in tupla:
+    print(num, end=' ')
+print(f'\nO menor valor na tupla é {min(tupla)} e o maior é {max(tupla)}.')
