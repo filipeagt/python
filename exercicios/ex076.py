950489253018cd0e6f57a6e2aaff12c96aa14580
Filipe Almeida <filipeagt.92@gmail.com>
2024-12-12 20:35:13 -0300

    Vídeo do desafio 76

--- exercicios/ex076.py
+++ exercicios/ex076.py
@@ -1,4 +1,14 @@
-listagem = ('Lápis', 1.75, 'Borracha', 2.0, 'Caderno', 15.90, 'Estojo', 25.0, 'Trasnsferidor', 4.20, 'Compasso', 9.99, 'Mochila', 120.32, 'Canetas', 22.30, 'Livro', 34.9)
+listagem = (
+    'Lápis', 1.75,
+    'Borracha', 2.0, 
+    'Caderno', 15.90, 
+    'Estojo', 25.0, 
+    'Trasnsferidor', 4.20, 
+    'Compasso', 9.99, 
+    'Mochila', 120.32, 
+    'Canetas', 22.30, 
+    'Livro', 34.9
+)
 print('-'*40)
 print(f'{"LISTAGEM DE PREÇOS":^40}')
 print('-'*40)
@@ -6,5 +16,5 @@ for indice in range(len(listagem)):
     if indice % 2 == 0:
         print(f'{listagem[indice]:.<30}', end='')
     else:
-        print(f'R${listagem[indice]:7.2f}')
+        print(f'R${listagem[indice]:7.2f}'.replace('.', ','))
 print('-'*40)
