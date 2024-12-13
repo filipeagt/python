2dabefe468ce08829c5d9f67f83171fa5b69501d
Filipe Almeida <filipeagt.92@gmail.com>
2024-12-13 14:07:40 -0300

    Desafio 83

---
+++ exercicios/ex083.py
@@ -0,0 +1,15 @@
+pilha = []
+expressao = input('Digite um expressão com parênteses: ')
+for caractere in expressao:
+    if caractere == '(':
+        pilha.append(caractere)
+    elif caractere == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(caractere)
+
+if len(pilha) == 0:
+    print(f'A expressão {expressao} é válida.')
+else:
+    print(f'A expressão {expressao} é inválida.')
