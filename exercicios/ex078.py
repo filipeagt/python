cd1a347ad83bb769be9cf0387737a5902056e09c
Filipe Almeida <filipeagt.92@gmail.com>
2024-12-13 15:30:13 -0300

    Vídeos desafios 78 a 80

--- exercicios/ex078.py
+++ exercicios/ex078.py
@@ -2,6 +2,7 @@ lista = []
 indiceAux = 0
 for x in range(5):
     lista.append(int(input(f'Digite um número para a posição {x}: ')))
+print('-='*30)
 print(f'Você digitou os valores {lista}')
 print(f'O maior valor digitado foi {max(lista)} na(s) posição(ões)', end=' ')
 for x in range(lista.count(max(lista))):
@@ -13,4 +14,3 @@ for x in range(lista.count(min(lista))):
     print(f'{lista.index(min(lista), indiceAux)}', end='... ')
     indiceAux = lista.index(min(lista), indiceAux) + 1
 print()
-#29:16
--- exercicios/ex080.py
+++ exercicios/ex080.py
@@ -1,17 +1,14 @@
 numeros = []
 for x in range(1, 6):
     num = int(input(f'Digite o {x}º número: '))
-    if x == 1:
+    if x == 1 or num > numeros[-1]:
         numeros.append(num)
-        print(f'Valor {num} Adicionado no final da lista...')
+        print(f'Valor {num} adicionado no final da lista...')
     else:
         for chave, valor in enumerate(numeros):
             if valor >= num:
                 numeros.insert(chave, num)
-                print(f'Valor {num} Adicionado na posição {chave} da lista...')
+                print(f'Valor {num} adicionado na posição {chave} da lista...')
                 break
-        if num > max(numeros):
-            numeros.append(num)
-            print(f'Valor {num} Adicionado no final da lista...')
 print('-='*30)
 print(f'Os valores digitados em ordem foram: {numeros}')
