db70d013f34c86ba3817324863931013835c781c
Filipe Almeida <filipeagt.92@gmail.com>
2024-12-13 08:54:00 -0300

    Desafio 78

---
+++ exercicios/ex078.py
@@ -0,0 +1,16 @@
+lista = []
+indiceAux = 0
+for x in range(5):
+    lista.append(int(input(f'Digite um número para a posição {x}: ')))
+print(f'Você digitou os valores {lista}')
+print(f'O maior valor digitado foi {max(lista)} na(s) posição(ões)', end=' ')
+for x in range(lista.count(max(lista))):
+    print(f'{lista.index(max(lista), indiceAux)}', end='... ')
+    indiceAux = lista.index(max(lista), indiceAux) + 1
+indiceAux = 0
+print(f'\nO menor valor digitado foi {min(lista)} na(s) posição(ões)', end=' ')
+for x in range(lista.count(min(lista))):
+    print(f'{lista.index(min(lista), indiceAux)}', end='... ')
+    indiceAux = lista.index(min(lista), indiceAux) + 1
+print()
+#29:16
