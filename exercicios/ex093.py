8a7c1bf1f997d73327ea75a2e50c86ef599587dd
Filipe Almeida <filipeagt.92@gmail.com>
2024-12-15 13:45:31 -0300

    Desafio 93

---
+++ exercicios/ex093.py
@@ -0,0 +1,18 @@
+jogador = {}
+partidas = []
+jogador['nome'] = input('Nome do jogador: ').strip()
+nPartidas = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+for x in range(nPartidas):
+    partidas.append(int(input(f'Quantos gols na partida {x}? ')))
+jogador['gols'] = partidas
+jogador['total'] = sum(partidas)
+print('-=' * 30)
+print(jogador)
+print('-=' * 30)
+for k, v in jogador.items():
+    print(f'O campo {k} tem o valor {v}.')
+print('-=' * 30)
+print(f'O jogador {jogador["nome"]} jogou {nPartidas} partidas.')
+for num, gols in enumerate(partidas):
+    print(f'    => Na partida {num}, fez {gols} gols.')
+print(f'Foi um total de {jogador["total"]} gols.')
