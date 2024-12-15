5075a6efb1e3de0a8681cd0553d1b32284ad37ac
Filipe Almeida <filipeagt.92@gmail.com>
2024-12-15 20:55:38 -0300

    Vídeos desafios 90 e 91

--- exercicios/ex090.py
+++ exercicios/ex090.py
@@ -1,6 +1,12 @@
 aluno = {}
 aluno['nome'] = input('Nome do aluno: ')
 aluno['média'] = float(input(f'Média de {aluno["nome"]}: '))
-aluno['situação'] = 'APROVADO' if aluno['média'] >= 7 else 'REPROVADO'
+if aluno['média'] >= 7:
+    aluno['situação'] = 'APROVADO'
+elif aluno['média'] >= 5:
+    aluno['situação'] = 'RECUPERAÇÃO'
+elif aluno['média'] < 5:
+    aluno['situação'] = 'REPROVADO'
+print('-=' * 30)
 for chave, valor in aluno.items():
-    print(f'{chave.capitalize()} é igual a {valor}')
+    print(f'  - {chave.capitalize()} é igual a {valor}')
