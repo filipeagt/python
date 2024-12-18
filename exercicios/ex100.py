b223bec9cdc13fb69360ecc2eaba73425802b5f6
Filipe Almeida <filipeagt.92@gmail.com>
2024-12-18 17:17:11 -0300

    Desafio 100

---
+++ exercicios/ex100.py
@@ -0,0 +1,24 @@
+from random import randint
+from time import sleep
+
+
+def sorteio(lista):
+    print('Sorteando 5 valores da lista: ', end='')
+    for x in range(5):
+        lista.append(randint(0, 10))
+        sleep(0.5)
+        print(f'{lista[x]}', end=' ', flush=True)
+    print('PRONTO!')
+
+
+def somaPar(lista):
+    s = 0    
+    for valor in lista:
+        if valor % 2 == 0:
+            s += valor
+    print(f'Somando os valores pares de {lista}, temos {s}.')
+
+    
+numeros = []
+sorteio(numeros)
+somaPar(numeros)
