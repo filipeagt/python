3206d0a3ab55c231b057fb5d626f43544b1082c3
Filipe Almeida <filipeagt.92@gmail.com>
2024-12-18 22:20:28 -0300

    Desafio 101

---
+++ exercicios/ex101.py
@@ -0,0 +1,20 @@
+from datetime import date
+
+
+def voto(nascimento):
+    idade = date.today().year - nascimento
+    texto = 'VOTO '
+    if idade < 16:
+        texto += 'NEGADO'
+    elif idade < 18:
+        texto += 'OPCIONAL'
+    elif idade < 65:
+        texto += 'OBRIGATÓRIO'
+    else:
+        texto += 'OPCIONAL'
+    return f'Com {idade} anos: {texto}.'
+
+
+print('-' * 40)
+ano = int(input('Em que ano você nasceu? '))
+print(voto(ano))
