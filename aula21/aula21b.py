e50a77c0218dc22a9ae109bf4ce1a62c40c0b501
Filipe Almeida <filipeagt.92@gmail.com>
2024-12-18 22:02:17 -0300

    Aula 21 Funções

---
+++ aula21/aula21b.py
@@ -0,0 +1,12 @@
+def somar(a=0, b=0, c=0):
+    """
+    => Faz a soma de três valores e mostra o resultado na tela.
+    :param a: o primeiro valor
+    :param b: o segundo valor
+    :param c: o terceiro valor
+    Função criadapor Filipe no Curso de Python
+    """
+    s = a + b + c
+    print(f'A soma vale {s}')
+
+somar(c=3, a=2)
---
+++ aula21/aula21c.py
@@ -0,0 +1,12 @@
+def teste():
+    x = 8
+    #n = 5
+    print(f'Na função teste, n vale {n}')
+    print(f'Na função teste, x vale {x}')
+
+
+# Programa Principal
+n = 2
+teste()
+print(f'No programa principal, n vale {n}')
+#print(f'No programa principal, x vale {x}')
