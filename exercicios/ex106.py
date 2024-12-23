065ac6f392d3b38f81bcd32d543eed69e671a283
Filipe Almeida <filipeagt.92@gmail.com>
2024-12-23 15:41:02 -0300

    Desafio 107

--- exercicios/ex106.py
+++ exercicios/ex106.py
@@ -1,3 +1,6 @@
+from time import sleep
+
+
 def escreva(texto, cor=''):
 	comprimento = len(texto) + 4
 	print(f'\033[{cor}m', end='')
@@ -5,18 +8,20 @@ def escreva(texto, cor=''):
 	print(f'  {texto}  ')
 	print('~' * comprimento)
 	print('\033[m', end='')
+	sleep(1)
 
 
 def ajuda():
 	while True:
-		escreva('Sistema de ajuda PyHELP', '7;32;47')
+		escreva('Sistema de ajuda PyHELP', '0;30;42')
 		palavra = input('Função ou Biblioteca > ').strip().lower()
 		if palavra == 'fim':
-			escreva('ATÉ LOGO!', '7;31;47')
+			escreva('ATÉ LOGO!', '0;30;41')
 			break
-		escreva(f"Acessando o manual do comando '{palavra}'", '7;34;47')
+		escreva(f"Acessando o manual do comando '{palavra}'", '0;30;44')
 		print('\033[7;30m', end='')
 		help(palavra)
 		print('\033[m', end='')
+		sleep(2)
 	
 ajuda()
---
+++ exercicios/ex107/moeda.py
@@ -0,0 +1,16 @@
+def aumentar(valor, percentual):
+	valor += valor * percentual / 100
+	return valor
+	
+
+def diminuir(valor, percentual):
+	valor -= valor * percentual / 100
+	return valor
+	
+	
+def metade(valor):
+	return valor / 2
+	
+	
+def dobro(valor):
+	return valor * 2
